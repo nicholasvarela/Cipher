e57cfba23a01461b347bd15fc48a84abe673d7e2
Nicholas Varela <nicholasjv@icloud.com>
2018-09-25 23:15:20 -0500

    First test

--- src/com/example/cipher/Cipher.java
+++ src/com/example/cipher/Cipher.java
@@ -39,7 +39,7 @@ public class Cipher {
         }
     }
     public static void main(String[] args) {
-
+        System.out.println("test");
         char  primaryCommand = 'r';
         while(primaryCommand == 'r' || primaryCommand == 'e' ) {
             System.out.println();
